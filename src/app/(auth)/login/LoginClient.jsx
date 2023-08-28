0583091350088a98c8b6d09ae2259f57f4c7062f
jintae1224 <kab7210@naver.com>
2023-08-28 22:32:40 +0900

    Input 컴포넌트 생성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 
 import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
+import Input from "@/components/input/Input";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -37,7 +38,28 @@ const LoginClient = () => {
             <Image priority src={LogoPath} alt="logo" />
           </h1>
           <form onSubmit={loginUser} className={styles.form}>
-            {/* input */}
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className={styles.group}>
               {/* 자동 로그인, 비밀번호 수정 */}
               자동 로그인, 비밀번호 수정
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,9 +1,94 @@
-const Input = () => {
-    return(
-        <div>
-            Input
-        </div>
-    )
-}
+import classNames from "classnames";
+import styles from "./Input.module.scss";
+import Icon from "../icon/Icon";
+import { useState } from "react";
 
-export default Input
+const Input = ({
+  id,
+  label,
+  name = "",
+  labelVisible,
+  icon,
+  email,
+  password,
+  placeholder = "",
+  readOnly,
+  disabled,
+  value,
+  error: errorProp,
+  className = "",
+  onChange,
+  ...restProps
+}) => {
+  const [inputValue, setInputValue] = useState(value ? value : "");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const checkType = () => {
+    if (email) {
+      return "email";
+    }
+    if (password) {
+      return isPasswordVisible ? "text" : "password";
+    }
+
+    return 'text'
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+    onChange(e);
+  };
+
+  const iconType = isPasswordVisible ? 'show' : 'hide';
+  const iconLabel = `비밀번호 ${isPasswordVisible ? '표시' : '감춤'}`
+
+  return (
+    <div className={classNames(styles.formControl, className)}>
+      <label
+        htmlFor={id}
+        className={classNames(styles.label, labelVisible || styles.labelHidden)}
+      >
+        {label}
+      </label>
+      <div
+        className={classNames(
+          styles.inputWrapper,
+          errorProp && styles.inputWrapperError
+        )}
+      >
+        {icon ? <Icon /> : null}
+        <input
+          id={id}
+          type={checkType()}
+          name={name}
+          className={classNames(styles.input)}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          disabled={disabled}
+          value={inputValue}
+          onChange={handleChange}
+          {...restProps}
+        />
+
+        {password ? (
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+            disabled={disabled}
+          >
+            <Icon type={iconType} alt={iconLabel} title={iconLabel} />
+          </button>
+        ) : null}
+
+        {errorProp ? (
+          <span role="alert" className={styles.error}>
+            {errorProp.message}
+          </span>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Input;

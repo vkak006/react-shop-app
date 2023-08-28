572cc24d00ae3e9cf8d3a6c5bc82f9f2e570de2a
jintae1224 <kab7210@naver.com>
2023-08-28 20:33:38 +0900

    Loader 컴포넌트 구현

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -1,59 +1,60 @@
-'use client'
-import Image from "next/image"
-import { useState } from "react"
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 
-import LogoPath from '@/assets/colorful.svg'
-import { useRouter } from "next/navigation"
+import LogoPath from "@/assets/colorful.svg";
+import { useRouter } from "next/navigation";
 
-import styles from './Auth.module.scss'
+import styles from "./Auth.module.scss";
+import Loader from "@/components/loader/Loader";
 
 const LoginClient = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [isAutoLogin, setIsAutoLogin] = useState(false);
+  const router = useRouter();
 
-    const router = useRouter()
+  const redirectUser = () => {
+    router.push("/");
+  };
 
-    const redirectUser = () => {
-        router.push('/');
-    }
+  const loginUser = (e) => {
+    // sumit evnet 발생 시 화면이 refresh 되는데 refresh를 방지
+    e.preventDefault();
+    setIsLoading(true);
+  };
 
-    const loginUser = (e) => {
-        // sumit evnet 발생 시 화면이 refresh 되는데 refresh를 방지
-        e.preventDefault();
-        setIsLoading(true)
-    }
+  const singInWithGoogle = () => {};
 
-    const singInWithGoogle = () => {
-
-    }
-
-    return (
-        <section className={styles.page}>
-            <div className={styles.container}>
-                <h1 className={styles.logo}>
-                    <Image priority src={LogoPath} alt="logo" />
-                </h1>
-                <form onSubmit={loginUser} className={styles.form}>
-                    {/* input */}
-                    <div className={styles.group}>
-                        {/* 자동 로그인, 비밀번호 수정 */}
-                        자동 로그인, 비밀번호 수정 
-                    </div>
-                    <div className={styles.buttonGroup}>
-                        {/* Button */}
-                        버튼
-                        <div>
-                            {/* Button */}
-                            버튼
-                        </div>
-                    </div>
-                </form>
+  return (
+    <>
+      <Loader />
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image priority src={LogoPath} alt="logo" />
+          </h1>
+          <form onSubmit={loginUser} className={styles.form}>
+            {/* input */}
+            <div className={styles.group}>
+              {/* 자동 로그인, 비밀번호 수정 */}
+              자동 로그인, 비밀번호 수정
             </div>
-        </section>
-    )
-}
+            <div className={styles.buttonGroup}>
+              {/* Button */}
+              버튼
+              <div>
+                {/* Button */}
+                버튼
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default LoginClient
+export default LoginClient;
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,9 +1,34 @@
-const Loader = () => {
-    return(
-        <div>
-            Loader
-        </div>
-    )
-}
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./Loader.module.scss";
 
-export default Loader
+const Loader = ({ basic }) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible={true}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;

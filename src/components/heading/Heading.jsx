c4aac6ea1b1e1f6bb2ac56916ca006f35fbb42a1
jintae1224 <kab7210@naver.com>
2023-09-03 19:23:47 +0900

    Heading 컴포넌트 생성

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,9 +1,16 @@
-const Heading = () => {
-    return(
-        <div>
-            Heading
-        </div>
-    )
-}
+import styles from "./Heading.module.scss";
 
-export default Heading
+const Heading = ({ title, subtitle, center }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={center ? styles.center : ""}>
+        <div className={styles.title}>{title}</div>
+      </div>
+      <div>
+        <div className={styles.subtitle}>{subtitle}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;

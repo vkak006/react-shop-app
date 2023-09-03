581d0726d44b3e2ce51937872bfe3d30b7067a45
jintae1224 <kab7210@naver.com>
2023-09-03 18:30:15 +0900

    회원가입 페이지 생성

---
+++ src/app/(auth)/register/RegisterClient.jsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import styles from "../login/Auth.module.scss";
+import Image from "next/image";
+import Button from "@/components/button/Button";
+
+import Divider from "@/components/divider/Divider";
+import Input from "@/components/input/Input";
+import { useState } from "react";
+
+import LogoPath from '@/assets/colorful.svg'
+import Link from "next/link";
+
+const RegisterClient = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
+  return (
+    <>
+      {/* <Loader /> */}
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image priority src={LogoPath} alt="logo" />
+          </h1>
+          <form onSubmit={registerUser} className={styles.form}>
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호 확인"
+              placeholder="비밀번호 확인"
+              className={styles.control}
+              value={cPassword}
+              onChange={(e) => setCPassword(e.target.value)}
+            />
+            <div className={styles.buttonGroup}>
+              {/* Button */}
+              <Button type="submit" width="100%">
+                회원가입
+              </Button>
+              <Divider />
+              <Button width="100%" secondary>
+                <Link href={"/login"}>로그인</Link>
+              </Button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default RegisterClient;
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,8 +1,10 @@
+import RegisterClient from "./RegisterClient"
+
+
 const Register = () => {
+
     return(
-        <div>
-            Register
-        </div>
+        <RegisterClient />
     )
 }
 

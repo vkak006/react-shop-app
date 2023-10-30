04b438f49200e4c7023323133aa0dae4c56c9611
jintae1224 <kab7210@naver.com>
2023-10-30 17:26:29 +0900

    --save

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,31 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/app";
-import { getFireStore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-import dotenv from 'dotenv'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-dotenv.config()
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: dotenv.apiKey,
-  authDomain: dotenv.authDomain,
-  projectId: dotenv.projectId,
-  storageBucket: dotenv.storageBucket,
-  messagingSenderId: dotenv.messagingSenderId,
-  appId: dotenv.appId,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFireStore(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
